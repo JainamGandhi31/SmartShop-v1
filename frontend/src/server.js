99d055f11bd6a5858c97ed281919f2134a2c1ce1
Jainam Gandhi <jainamgandhi02@gmail.com>
2023-05-06 19:37:12 +0530

    updated path for server and images

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000/"
+export const server = "https://smart-shop-v1-ye6u.vercel.app/api/v2";
+export const backend_url = "https://smart-shop-v1-ye6u.vercel.app/"
